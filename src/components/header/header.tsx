cbc637c43e082ed197b7fc99c54f962531603201
VoDucHuy <voduchuy542@gmail.com>
2023-11-20 08:40:03 +0700

    MAIN [HUY Remove header widgets, Change axios payload to fullname]

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,43 +2,43 @@ import { ReactComponentElement } from "react";
 import "./header.css";
 import { Link, NavLink } from "react-router-dom";
 
-const activeNavList = [{
+const activeNavList = [
+  {
     content: <>Home</>,
-    path: "/"
-}, {
-    content: <>About</>,
-    path: "/about"
-}, {
-    content: <>Blog</>,
-    path: "/blog"
-}]
+    path: "/",
+  },
+];
 
-function ActiveNavLink({ children, path }: { children: ReactComponentElement<any>, path: string }) {
-    return <NavLink
-        to={path}
-        className={({ isActive }) =>
-            isActive ? "active" : ""
-        }>
-        {children}
+function ActiveNavLink({
+  children,
+  path,
+}: {
+  children: ReactComponentElement<any>;
+  path: string;
+}) {
+  return (
+    <NavLink to={path} className={({ isActive }) => (isActive ? "active" : "")}>
+      {children}
     </NavLink>
+  );
 }
 
 export default function Header() {
-    const navList = activeNavList.map(nav => <ActiveNavLink children={nav.content} path={nav.path} key={nav.path} />)
-    return <div className="header">
-        <ul className="nav nav-list">
-            {navList}
-        </ul>
+  const navList = activeNavList.map((nav) => (
+    <ActiveNavLink children={nav.content} path={nav.path} key={nav.path} />
+  ));
+  return (
+    <div className="header">
+      <ul className="nav nav-list">{navList}</ul>
 
-        <ul className="nav nav-pages">
-            <ActiveNavLink path="/signin">
-                <li className="primary">
-                    Sign In
-                </li>
-            </ActiveNavLink>
-            <li className="primary register">
-                <Link to="signup">Register</Link>
-            </li>
-        </ul>
-    </div >
+      <ul className="nav nav-pages">
+        <ActiveNavLink path="/signin">
+          <li className="primary">Sign In</li>
+        </ActiveNavLink>
+        <li className="primary register">
+          <Link to="signup">Register</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
